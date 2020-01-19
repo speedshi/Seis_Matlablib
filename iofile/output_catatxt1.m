e958a776d7db7c5e4674ab74230d0e1d095557f9
speedshi <speedshi@hotmail.com>
2020-01-19 22:11:56 +0100

    correct bugs.

--- iofile/output_catatxt1.m
+++ iofile/output_catatxt1.m
@@ -1,11 +1,12 @@
 function output_catatxt1(catalog,fname,para)
 % This function is used to output the catalog in text format.
 % The output file can then be used for GMT plotting.
+% NOTE: the output unit is exactly the same as the input unit, no transfer here.
 %
 % INPUT--------------------------------------------------------------------
 %       catalog: structure, contains catalog information, e.g.
 %       catalog.time, catalog.magnitude, catalog.latitude,
-%       catalog.longitude, catalog.depth...
+%       catalog.longitude, catalog.depth, catalog.distance...
 %       fname: string, filename for the output file;
 %       para: structure, contains parameters to format the output file;
 %
@@ -14,34 +15,42 @@ function output_catatxt1(catalog,fname,para)
 % set default parameters
 if nargin < 2
     fname='catalog.txt';
-    para=[];
+    para.rftime='toordinal';
+    para.distance=false;
 end
 
 
 n=length(catalog.time); % number of earthquakes in the input catalog
 
-if ~isempty(para)
-    if isfield(para,'rftime')
-        if strcmp(para.rftime,'toordinal')
-            % relative days count from the date AD 01/01/01, i.e.
-            % equivalent to python datetime.toordinal() function.
-            rtimes=datenum(catalog.time)-366;
-        else
-            % use days relative to a input datetime
-            rtimes=days(catalog.time-para.rftime);
-        end
-    end
-    
+
+
+if strcmp(para.rftime,'toordinal')
+    % relative days count from the date AD 01/01/01, i.e.
+    % equivalent to python datetime.toordinal() function.
+    rtimes=datenum(catalog.time)-366;
+else
+    % use days relative to a input datetime
+    rtimes=days(catalog.time-para.rftime);
 end
+
 
 
 fid=fopen(fname,'wt');
 
 % output the text file
-for ii=1:n
-    % note the format of the output file: longitude-latitude-depth-origin
-    % times-magnitude
-    fprintf(fid,'%f %f %f %f %f \n',catalog.longitude(ii),catalog.latitude(ii),catalog.depth(ii),rtimes(ii),catalog.magnitude(ii));
+if para.distance
+    % output distances, the catalog must contain distance information
+    for ii=1:n
+        % note the format of the output file:
+        % longitude-latitude-depth-origin times-magnitude-distances
+        fprintf(fid,'%f %f %f %f %f %f \n',catalog.longitude(ii),catalog.latitude(ii),catalog.depth(ii),rtimes(ii),catalog.magnitude(ii),catalog.distance(ii));
+    end
+else
+    % do not output distances
+    for ii=1:n
+        % note the format of the output file: longitude-latitude-depth-origin times-magnitude
+        fprintf(fid,'%f %f %f %f %f \n',catalog.longitude(ii),catalog.latitude(ii),catalog.depth(ii),rtimes(ii),catalog.magnitude(ii));
+    end
 end
 
 fclose(fid);
--- seismology/catana_dist.m
+++ seismology/catana_dist.m
@@ -1,37 +1,65 @@
-function catana_dist(file,earthquake)
+function catana_dist(catalog,earthquake,para)
 % This function is used to analyse the catalog.
 % Show the distances of events in the catalog with respect to the input earthquake.
 %
 % INPUT--------------------------------------------------------------------
-% file: string, file name of the catalog;
+% catalog: structure, the catalog of seismic events, unit: meter and degree;
 % earthquake: structure, information of the input earthquake;
 % earthquake.latitude: latitude in degree of the input earthquake;
 % earthquake.longitude: longitude in degree of the input earthquake;
 % earthquake.elevation: elevation in meter of the input earthquake;
+% para: structure, contain parameters to adjust the plot;
+% para.depth: if use color to respresent event depth;
+% para.newfig: if plot on a new figure;
 %
 % OUTPUT-------------------------------------------------------------------
 % figure showing the distance.
+
+% set default parameters
+if nargin<3
+    para.depth=true;
+    para.newfig=true;
+end
+
+if ~isfield(para,'depth')
+    para.depth=true;
+end
+
+if ~isfield(para,'newfig')
+    para.newfig=true;
+end
 
 
 % obtain the coordinates of the input earthquake
 [east,north,depth]=geod2cart(earthquake.latitude,earthquake.longitude,earthquake.elevation);
 
-% read in the catalog data
-catalog=read_catalog(file);
-
-
 % calculate the distances between the events and the input earthquake
 dist=sqrt((catalog.east-east).^2+(catalog.north-north).^2)/1000; % note the unit is now in km
 
-sz=dnormlz(catalog.magnitude,4,12); % magnitude of events control the relative sizes of the ploted dots
+sz=dnormlz(catalog.magnitude,6,20); % magnitude of events control the relative sizes of the ploted dots
 
 % plot the distances
-figure;
-scatter(catalog.time,dist,sz,catalog.depth/1000,'filled');
-cb=colorbar; cb.Label.String='Event depth (km)';
+if para.newfig
+    figure;
+end
+
+hold on;
+
+if para.depth
+    scatter(catalog.time,dist,sz,catalog.depth/1000,'filled','MarkerFaceAlpha',0.4,'MarkerEdgeAlpha',0.4);
+    cb=colorbar; cb.Label.String='Event depth (km)';
+else
+    scatter(catalog.time,dist,sz,'k','filled','MarkerFaceAlpha',0.4,'MarkerEdgeAlpha',0.4);
+end
 axis tight;
-xlabel('Time');
-ylabel('Distance from epicenter (km)');
-    
+ax=gca;
+ax.FontSize=14;
+xlabel('Time','fontsize',16);
+xtickformat('yyyy-MM');
+ax.XAxis.MinorTick = 'on';
+set(ax,'TickLength',[0.012 0.02]);
+ax.LineWidth=1.3;
+ylabel('Distance from epicenter (km)','fontsize',16);
+
 
 end
